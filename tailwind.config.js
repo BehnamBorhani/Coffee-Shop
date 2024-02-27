00684156b3289d7c0463fa82f5a3a2857a6b2798
Behnam <behnamborhani95@gmail.com>
2024-02-27 22:58:26 +0330

    config tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,77 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
+      },
+      boxShadow: {
+        normal: "0px 1px 10px rgba(0,0,0,0.5);",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana Medium",
+        DanaDemibold: "Dana Demibold",
+        MorabbaLight: "Morabba Light",
+        MorabbaMedium: "Morabba Medium",
+        MorabbaBold: "Morabba Bold",
+      },
+      letterSpacing: {
+        tightest: "-0.065em",
+      },
+      spacing: {
+        4.5: "1.125rem",
+        25: "6.25rem",
+        30: "7.5rem",
+        50: "12.5rem",
+      },
+      container: {
+        center: true,
+        paddingTop: {
+          DEFAULT: "1rem", // 16px
+          lg: "0.625", // 10px
+        },
+      },
+      backgroundImage: {
+        "header-mobile": "url(../images/headerBgMobile.webp)",
+        "header-desktop": "url(../images/headerBgDesktop.webp)",
+      },
+      backgroundSize: {
+        100: "100%",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      // '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "&>*");
+      addVariant("child-hover", "&>*:hover");
+    },
+  ],
+};
