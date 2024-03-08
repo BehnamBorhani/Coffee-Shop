5015da9d5ddc631cbbb07a29fd0be8b08826f645
Behnam <behnamborhani95@gmail.com>
2024-03-08 18:44:41 +0330

    coffee club section created

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Header from "./_components/header/header";
 import ProductCard from "./_components/product-card/product-card";
 import Link from "next/link";
 import BestSellingProducts from "./_components/best-selling-products/best-selling-products";
+import Image from "next/image";
 
 export default function Home() {
   const { theme } = useAppContext();
@@ -163,6 +164,70 @@ export default function Home() {
             </div>
 
             <BestSellingProducts />
+          </div>
+        </section>
+
+        <section class="coffee-club mb-8 md:mb-20 ">
+          <div class="container">
+            <div class="flex items-center flex-wrap lg:flex-nowrap md:gap-x-3 xl:gap-x-24 gap-y-9 lg:h-36 py-8 lg:py-0 px-3 lg:px-5 xl:px-11 text-white bg-gradient-to-r from-emerald-500 to-emerald-600 rounded-2xl">
+              <div class="flex items-center md:shrink-0 gap-x-3 lg:gap-x-4 xl:gap-x-6 ">
+                <Image
+                  src="/images/club/diamond.png"
+                  width={87}
+                  height={77.5}
+                  className="w-87 md:w-[110px]"
+                />
+                <div>
+                  <h4 class="font-MorabbaBold text-2xl md:text-5xl mb-2">
+                    کافی کلاب
+                  </h4>
+                  <p class="font-MorabbaLight text-lg md:text-2xl">
+                    میدونستی میتونی با امتیازهات قهوه بگیری ؟
+                  </p>
+                </div>
+              </div>
+
+              <div class="flex justify-between w-full">
+                <div class="flex items-center gap-x-2 lg:gap-x-3 xl:gap-x-5">
+                  <div class="w-[72px] h-[72px] md:w-[98px] md:h-[98px] text-center text-emerald-600 bg-white py-1.5 md:pt-5 md:pb-1 rounded-2xl">
+                    <svg class="w-10 h-10 md:w-12 md:h-12 mb-1 md:mb-1.5 mx-auto">
+                      <use href="#activity"></use>
+                    </svg>
+                    <span class="text-xs md:text-sm">چرخ و بخت</span>
+                  </div>
+
+                  <div class="w-[72px] h-[72px] md:w-[98px] md:h-[98px] text-center text-emerald-600 bg-white py-1.5 md:pt-5 md:pb-1 rounded-2xl">
+                    <svg class="w-10 h-10 md:w-12 md:h-12 mb-1 md:mb-1.5 mx-auto">
+                      <use href="#discovery"></use>
+                    </svg>
+                    <span class="text-xs md:text-sm">ماموریت ها</span>
+                  </div>
+
+                  <div class="w-[72px] h-[72px] md:w-[98px] md:h-[98px] text-center text-emerald-600 bg-white py-1.5 md:pt-5 md:pb-1 rounded-2xl">
+                    <svg class="w-10 h-10 md:w-12 md:h-12 mb-1 md:mb-1.5 mx-auto">
+                      <use href="#ticket-star"></use>
+                    </svg>
+                    <span class="text-xs md:text-sm">جایزه ها</span>
+                  </div>
+                </div>
+
+                <div class="flex flex-col">
+                  <span class="md:mb-1 font-DanaDemibold text-2xl md:text-3xl">
+                    542
+                  </span>
+                  <span class="text-xs md:text-sm">امتیــاز شما</span>
+                  <a
+                    href="#"
+                    class="flex items-center justify-center w-[90px] h-[26px] md:w-[110px] md:h-8 mt-1 md:mt-2 font-DanaMedium text-xs md:text-sm rounded-full bg-gradient-to-r from-orange-200  to-orange-300"
+                  >
+                    <span>دریافت جایزه</span>
+                    <svg class="w-5 h-5 md:w-6 md:h-6">
+                      <use href="#chevron-left-mini"></use>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </main>
